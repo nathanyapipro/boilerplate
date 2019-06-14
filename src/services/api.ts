ad467cc18d0f61455876a267d30e1a9a18547bc4
Nathan Yapi <nathanyapi.pro@gmail.com>
2019-06-13 20:41:10 -0400

    feat(api): initial fetch implementation of api

--- src/services/api.ts
+++ src/services/api.ts
@@ -72,55 +72,129 @@ export function instanceOfHttpStatusForbidden(err?: Error): boolean {
   );
 }
 
+export interface HttpResponse<T> extends Response {
+  parsedBody: T;
+}
+
+export const http = <T>(request: RequestInfo): Promise<HttpResponse<T>> => {
+  return new Promise((resolve, reject) => {
+    let response: Response;
+    let parsedResponse: HttpResponse<T>;
+    fetch(request)
+      .then(res => {
+        response = res;
+        return res.json();
+      })
+      .then(body => {
+        if (response.ok) {
+          parsedResponse = {
+            ...response,
+            parsedBody: body
+          };
+          resolve(parsedResponse);
+        } else {
+          reject(response);
+        }
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+};
+
 export class ApiClient {
   axiosInstance: AxiosInstance;
+  token?: string;
+  urlBase: string;
 
   constructor(axiosInstance: AxiosInstance) {
     this.axiosInstance = axiosInstance;
+    this.urlBase = process.env.REACT_APP_BACKEND_URL || "localhost:8080";
   }
 
   setAuthorizationHeader(token: string) {
-    this.axiosInstance.defaults.headers.common.Authorization = token;
+    this.token = token;
   }
 
   removeAuthorizationHeader() {
-    this.axiosInstance.defaults.headers.common.Authorization = undefined;
+    this.token = undefined;
   }
 
-  getConfig(): AxiosRequestConfig {
+  getHeaders(): any {
     const plateformKey = "cf053c9deaed22f035261bde50c1d6e8";
     const uuid = getLocalStorageAuthUuid();
-    if (!uuid) {
-      return {};
-    }
     const time = Math.floor(+new Date() / 1000) + "";
     const arr = [plateformKey, time, uuid];
     const temp = arr.sort().join("");
     const apiKey = sha1(temp);
 
     return {
-      headers: {
-        apiKey: apiKey,
-        time: time,
-        uuid: uuid,
-        version: "cms1"
-      }
+      Authorization: this.token,
+      "Content-Type": "application/json",
+      apiKey: apiKey,
+      time: time,
+      uuid: uuid,
+      version: "cms1"
     };
   }
 
+  get = async <T>(
+    path: string,
+    args: RequestInit = {
+      method: "get",
+      headers: this.getHeaders()
+    }
+  ): Promise<HttpResponse<T>> => {
+    return await http<T>(new Request(path, args));
+  };
+
+  post = async <T>(
+    path: string,
+    body: any,
+    args: RequestInit = {
+      method: "post",
+      body: JSON.stringify(body),
+      headers: this.getHeaders()
+    }
+  ): Promise<HttpResponse<T>> => {
+    return await http<T>(new Request(path, args));
+  };
+
+  put = async <T>(
+    path: string,
+    body: any,
+    args: RequestInit = {
+      method: "put",
+      body: JSON.stringify(body),
+      headers: this.getHeaders()
+    }
+  ): Promise<HttpResponse<T>> => {
+    return await http<T>(new Request(path, args));
+  };
+
   async login(params: ApiLoginParams): Promise<ApiLoginResponse> {
-    const url = `/cms/account/login`;
-    const config = this.getConfig();
-    console.log(config);
-    const response = await this.axiosInstance.post<ApiLoginResponse>(
-      url,
-      JSON.stringify(params),
-      config
+    const response = await this.post<ApiLoginResponse>(
+      `${this.urlBase}/cms/account/login`,
+      params
     );
-    const { appToken } = response.data;
-    this.setAuthorizationHeader(appToken);
-    return response.data;
+
+    const data = response.parsedBody;
+    this.setAuthorizationHeader(data.appToken);
+    return data;
   }
+
+  // async login(params: ApiLoginParams): Promise<ApiLoginResponse> {
+  //   const url = `/cms/account/login`;
+  //   const config = this.getConfig();
+  //   const response = await this.axiosInstance.post<ApiLoginResponse>(
+  //     url,
+  //     JSON.stringify(params),
+  //     config
+  //   );
+  //   const { appToken } = response.data;
+  //   this.setAuthorizationHeader(appToken);
+  //   return response.data;
+  // }
 
   async logout(): Promise<ApiLogoutResponse> {
     const url = `/v1/logout`;
